41410632928c08a2f81585ea6824763ee3ad0b87
Jeremy Duncan <jduncan46@my.devry.edu>
2023-10-09 21:08:39 -0400

    implemented Explosion class

--- app/javascript/game_scene.js
+++ app/javascript/game_scene.js
@@ -2,6 +2,7 @@
 import Alien      from './Alien';       // Import the Alien class
 import Background from './Background';  // Import the Background class
 import Laser      from './Laser';       // Import the Laser class
+import Explosion  from './Explosion';   // Import the Explosion class
 import Scoreboard from './Scoreboard';  // Import the Scoreboard class
 
 class GameScene extends Phaser.Scene {
@@ -23,32 +24,21 @@ class GameScene extends Phaser.Scene {
   // Create function to set up the game scene
   create() {
     // Initialize classes
-    this.background = new Background(this);  // Create an instance of the Background class
-    this.alien      = new Alien(this);       // Create an instance of the Alien class
-    this.laser      = new Laser(this);       // Create an instance of the Laser class
-    this.scoreboard = new Scoreboard(this);  // Initialize the Scoreboard class
+    this.background = new Background(this);
+    this.alien      = new Alien(this);
+    this.laser      = new Laser(this);
+    this.explosion  = new Explosion(this);
+    this.scoreboard = new Scoreboard(this);
 
     // Initialize arrays and groups
     this.starsGraphics = [];
-    this.explosions = this.add.group();
-
-    // Define the explosion animation
-    this.anims.create({
-      key: 'explode',
-      frames: this.anims.generateFrameNumbers('explosion', { start: 3, end: 63 }),
-      frameRate: 60,
-      repeat: 0,
-      hideOnComplete: true
-    });
-
-    // Create and set up the player's spaceship
     this.spaceship = this.physics.add.sprite(this.scale.width / 2, this.scale.height / 2, 'spaceship');
     this.spaceship.setScale(0.4);
     this.cursors = this.input.keyboard.createCursorKeys();
     this.spaceshipSpeed = 5;
-    this.lasers   = this.physics.add.group(); // Create a group for lasers
-    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);  // Create a key object for the spacebar
-    this.alienSpaceships = this.physics.add.group();  // Create a group for alien spaceships
+    this.lasers = this.physics.add.group();
+    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.alienSpaceships = this.physics.add.group();
 
     // Create a text object for the "Game Over" message but set it to be invisible initially
     this.gameOverText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Game Over', {
@@ -58,45 +48,30 @@ class GameScene extends Phaser.Scene {
   }
 
   update() {
-    // =========================
     // Handle spaceship rotation
-    // -------------------------
     if (this.cursors.left.isDown) {
       this.spaceship.angle -= 3;
     }
     if (this.cursors.right.isDown) {
       this.spaceship.angle += 3;
     }
-    // =========================
+
     // Handle spaceship movement
-    // -------------------------
     if (this.cursors.up.isDown) {
       const angleInRad = Phaser.Math.DegToRad(this.spaceship.angle);
       const dx = this.spaceshipSpeed * Math.cos(angleInRad);
       const dy = this.spaceshipSpeed * Math.sin(angleInRad);
-      // =================================================
+
       // Update star positions based on spaceship movement
-      // -------------------------------------------------
       this.background.updateStars(dx, dy);
-      // ==============================
+
       // Update positions of explosions
-      // ==============================
-      this.explosions.getChildren().forEach(explosion => {
-        explosion.x -= dy;
-        explosion.y += dx;
-        // =================================
-        // Wrap explosions around the screen
-        // ---------------------------------
-        if (explosion.x < 0) explosion.x    += 1600;
-        if (explosion.x > 1600) explosion.x -= 1600;
-        if (explosion.y < 0) explosion.y    += 1200;
-        if (explosion.y > 1200) explosion.y -= 1200;
-      });
+      this.explosion.updateExplosions(dx, dy);
     }
 
-    this.background.randomizeAlpha(); // Randomly change the alpha value of stars
-    this.laser.fire(this.spacebar, this.spaceship, this.spaceship.angle); // Handle laser firing using Laser class
-    this.laser.destroyOffScreen(); // Destroy lasers that go off-screen using Laser class
+    this.background.randomizeAlpha();
+    this.laser.fire(this.spacebar, this.spaceship, this.spaceship.angle);
+    this.laser.destroyOffScreen();
 
     // Handle alien spaceship spawning
     if (Phaser.Math.Between(0, 100) > 95) {
@@ -108,28 +83,21 @@ class GameScene extends Phaser.Scene {
 
     // Collision handling between lasers and alien spaceships
     this.physics.overlap(this.laser.getLasers(), this.alien.getAlienSpaceships(), (laser, alienSpaceship) => {
-      console.log("Collision detected");  // Debugging line
-
       laser.destroy();
       alienSpaceship.destroy();
 
       // Create an explosion at the collision point
-      const explosion = this.add.sprite(alienSpaceship.x, alienSpaceship.y, 'explosion');
-      explosion.play('explode');
-      this.explosions.add(explosion);  // Add the explosion to the group
-      this.scoreboard.updateScore(10); // Update the score using Scoreboard class
+      this.explosion.createExplosion(alienSpaceship.x, alienSpaceship.y);
+      this.scoreboard.updateScore(10);
     });
 
     // Handle collisions between the player's spaceship and alien spaceships
     this.physics.overlap(this.spaceship, this.alien.getAlienSpaceships(), (spaceship, alienSpaceship) => {
-      const alienExplosion = this.add.sprite(alienSpaceship.x, alienSpaceship.y, 'explosion');
-      alienExplosion.play('explode');
-      this.explosions.add(alienExplosion);
-      alienSpaceship.destroy();
+      // Create explosions for both the alien and the player
+      this.explosion.createExplosion(alienSpaceship.x, alienSpaceship.y);
+      this.explosion.createExplosion(spaceship.x, spaceship.y);
 
-      const playerExplosion = this.add.sprite(spaceship.x, spaceship.y, 'explosion');
-      playerExplosion.play('explode');
-      this.explosions.add(playerExplosion);
+      alienSpaceship.destroy();
       spaceship.destroy();
 
       // Update the player's score in the database and display the "Game Over" text
@@ -145,8 +113,6 @@ class GameScene extends Phaser.Scene {
     });
   }
 }
+
 // Export the GameScene class for use in other files
 export default GameScene;
-
-
-
