d6704a49dc588869673d4100ee5eb264938cfbba
Jeremy Duncan <jduncan46@my.devry.edu>
2023-10-10 22:54:56 -0400

    updated collision handler

--- app/javascript/CollisionHandler.js
+++ app/javascript/CollisionHandler.js
@@ -1,9 +1,10 @@
 // CollisionHandler.js
 export default class CollisionHandler {
-  constructor(scene, laser, redLaser, alien, spaceship, explosion, scoreboard, particleManager) {
+  constructor(scene, laser, redLaser, superLaser, alien, spaceship, explosion, scoreboard, particleManager) {
     this.scene      = scene;
     this.laser      = laser;
     this.redLaser   = redLaser;
+    this.superLaser = superLaser;
     this.alien      = alien;
     this.explosion  = explosion;
     this.scoreboard = scoreboard;
@@ -11,65 +12,80 @@ export default class CollisionHandler {
     this.gemSound   = this.scene.sound.add('gemSound');
     this.particles  = this.scene.particles;
     this.particleManager = particleManager;
-
   }
 
   handleCollisions() {
+    // =========================================================
     // Handle collisions between the player's spaceship and gems
+    // =========================================================
     this.scene.physics.overlap(this.spaceship, this.alien.gems, (spaceship, gem) => {
       gem.destroy();
-
-      // Play the gem pickup sound
       this.gemSound.play();
-
-
       // Start the particle emitters using ParticleManager
       this.particleManager.startEmitter('emitter', spaceship.x, spaceship.y);
       this.particleManager.startEmitter('mainEmitter', spaceship.x, spaceship.y);
       this.particleManager.startEmitter('secondEmitter', spaceship.x, spaceship.y);
-
       // Stop the emitter after a short time
       this.scene.time.delayedCall(500, () => {
         this.particleManager.stopEmitter('emitter');
         this.particleManager.stopEmitter('mainEmitter');
         this.particleManager.stopEmitter('secondEmitter');
       });
-
       this.scoreboard.updateScore(5);
     });
 
-
-    this.scene.physics.overlap(this.laser.getLasers(), this.alien.getAlienSpaceships(), (laser, alienSpaceship) => {
-      laser.destroy();
+    // ============================================================
+    // Handle collisions between the player's Super laser and Alien
+    // ============================================================
+    this.scene.physics.overlap(this.superLaser.getLasers(), this.alien.getAlienSpaceships(), (laser, alienSpaceship) => {
       alienSpaceship.destroy();
-
-      // Create an explosion at the collision point
       this.explosion.createExplosion(alienSpaceship.x, alienSpaceship.y);
-      this.scoreboard.updateScore(10);
-
-      // Drop a gem
+      this.scoreboard.updateScore(20);
       this.alien.dropGem(alienSpaceship.x, alienSpaceship.y);
 
+      laser.hitCount = (laser.hitCount || 0) + 1;
+      // Destroy laser if it has hit 2 aliens
+      if (laser.hitCount >= 4) {
+        laser.destroy();
+      }
     });
 
+
+    // =========================================================
+    // Handle collisions between the player's laser and Alien
+    // =========================================================
+    this.scene.physics.overlap(this.laser.getLasers(), this.alien.getAlienSpaceships(), (laser, alienSpaceship) => {
+      alienSpaceship.destroy();
+      this.explosion.createExplosion(alienSpaceship.x, alienSpaceship.y);
+      this.scoreboard.updateScore(10);
+      this.alien.dropGem(alienSpaceship.x, alienSpaceship.y);
+
+      laser.hitCount = (laser.hitCount || 0) + 1;
+      // Destroy laser if it has hit 2 aliens
+      if (laser.hitCount >= 2) {
+        laser.destroy();
+      }
+    });
+
+
+    // ==========================================================
+    // Handle collisions between the player's red laser and Alien
+    // ==========================================================
     this.scene.physics.overlap(this.redLaser.getLasers(), this.alien.getAlienSpaceships(), (laser, alienSpaceship) => {
       laser.destroy();
       alienSpaceship.destroy();
-
-      // Create an explosion at the collision point
       this.explosion.createExplosion(alienSpaceship.x, alienSpaceship.y);
       this.scoreboard.updateScore(10);
-
-      // Drop a gem
       this.alien.dropGem(alienSpaceship.x, alienSpaceship.y);
     });
 
+    // =====================================================================
     // Handle collisions between the player's spaceship and alien spaceships
+    // =====================================================================
     this.scene.physics.overlap(this.spaceship, this.alien.getAlienSpaceships(), (spaceship, alienSpaceship) => {
       // Create explosions for both the alien and the player
       this.explosion.createExplosion(alienSpaceship.x, alienSpaceship.y);
       this.explosion.createExplosion(spaceship.x, spaceship.y);
-
       alienSpaceship.destroy();
       spaceship.destroy();
 
@@ -85,9 +101,9 @@ export default class CollisionHandler {
     });
   }
 
-  // ==============================================================================
-// Add this method to your CollisionHandler class to handle particle collisions
-// ------------------------------------------------------------------------------
+  // ==========================
+  // handle particle collisions
+  // --------------------------
   handleParticleCollisions() {
     this.scene.physics.overlap(this.particleManager.getParticles(), this.alien.getAlienSpaceships(), (particle, alienSpaceship) => {
       alienSpaceship.destroy();
